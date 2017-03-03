043867b4d772202050bec366ef5479c2ac0275a6
Ian Fade <giewev@gmail.com>
2017-03-02 23:27:33 -0500

    Return basic objects as sane json structures

--- Server/Main.py
+++ Server/Main.py
@@ -36,26 +36,47 @@ c.execute('''CREATE TABLE IF NOT EXISTS user_events(
 def hello():
 	return "Hello World";
 
+def build_user_from_row(query_row, contact_visible):
+	user = dict()
+	user["name"] = query_row[0]
+	user["username"] = query_row[1]
+	if contact_visible:
+		user["contact_info"] = query_row[3]
+
+	user["interests"] = []
+	user["events"] = []
+	user["friends"] = []
+	return user
+
+def build_topic_from_row(query_row):
+	topic = dict()
+	topic["name"] = query_row[0]
+	return topic
+
+def build_event_from_row(query_row):
+	event = dict()
+	event["topic_id"] = query_row[0]
+	event["name"] = query_row[1]
+	event["description"] = query_row[2]
+	event["start_time"] = query_row[3]
+
 @app.route("/users")
 def get_users():
 	users = c.execute("SELECT * FROM users")
-	user_rows = [x for x in users]
+	user_rows = [build_user_from_row(x, False) for x in users]
 	return json.dumps(user_rows)
 
 @app.route("/topics")
 def get_topics():
 	topics = c.execute("SELECT * FROM topics")
-	topic_rows = [x for x in topics]
+	topic_rows = [build_topic_from_row(x) for x in topics]
 	return json.dumps(topic_rows)
 
 @app.route("/events")
 def get_events():
-	events = c.execute("SELECT * FROM topic_event")
-	event_rows = [x for x in events]
+	events = c.execute("SELECT * FROM topic_events")
+	event_rows = [build_event_from_row(x) for x in events]
 	return json.dumps(event_rows)
-
-@app.route("")
-def 
 
 def insert_test_data():
 	test_users = [	('Ian Fade', 'giewev', 1, '444-444-4444'),
