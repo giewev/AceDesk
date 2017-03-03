2860ccdce6a1eff81a2f73ff7cd545e8efa4a063
Ian Fade <giewev@gmail.com>
2017-03-02 22:22:38 -0500

    Add basic SQLITE backend

--- Server/Main.py
+++ Server/Main.py
@@ -1,5 +1,36 @@
 from flask import Flask, request
+import sqlite3
 app = Flask(__name__)
+conn = sqlite3.connect('data.db')
+
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS users(
+             ID   				integer        	NOT NULL,
+			 NAME 				text (255)     	NOT NULL,
+			 CONTACT_INFO  		text (255))''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS topics(
+             ID   				integer        	NOT NULL,
+			 NAME 				text (255)     	NOT NULL)''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS user_interests(
+             USER_ID   			integer        	NOT NULL,
+			 TOPIC_ID 			text (255)     	NOT NULL)''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS friendships(
+             VISIBLE_USER_ID 	integer        	NOT NULL,
+			 ALLOWED_USER_ID 	text (255)     	NOT NULL)''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS topic_events(
+             TOPIC_ID   		integer        	NOT NULL,
+			 EVENT_ID 			text (255)    	NOT NULL,
+			 NAME 				text (255)		NOT NULL,
+			 DESCRIPTION		text (255)		NOT NULL,
+			 START_TIME			text (255)		NOT NULL)''')
+
+c.execute('''CREATE TABLE IF NOT EXISTS user_events(
+             EVENT_ID   		integer        	NOT NULL,
+			 USER_ID 			integer    		NOT NULL)''')
 
 @app.route("/hello")
 def hello():
