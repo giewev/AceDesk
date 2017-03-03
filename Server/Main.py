de8093af971ff8764bcd6e7f3e76eb7bf65951ed
Ian Fade <giewev@gmail.com>
2017-03-03 00:46:58 -0500

    Add cross referencing for things that users are involved with

--- Server/Main.py
+++ Server/Main.py
@@ -22,11 +22,11 @@ c.execute('''CREATE TABLE IF NOT EXISTS topic_events(
 
 c.execute('''CREATE TABLE IF NOT EXISTS user_interests(
              USER_ID   			integer        	NOT NULL,
-			 TOPIC_ID 			text (255)     	NOT NULL)''')
+			 TOPIC_ID 			integer     	NOT NULL)''')
 
 c.execute('''CREATE TABLE IF NOT EXISTS friendships(
              VISIBLE_USER_ID 	integer        	NOT NULL,
-			 ALLOWED_USER_ID 	text (255)     	NOT NULL)''')
+			 ALLOWED_USER_ID 	integer     	NOT NULL)''')
 
 c.execute('''CREATE TABLE IF NOT EXISTS user_events(
              EVENT_ID   		integer        	NOT NULL,
@@ -43,10 +43,36 @@ def build_user_from_row(query_row, contact_visible):
 	if contact_visible:
 		user["contact_info"] = query_row[3]
 
-	user["interests"] = []
-	user["events"] = []
-	user["friends"] = []
+	add_metadata_to_user(user)
 	return user
+
+def add_metadata_to_user(user):
+	user_interests = c.execute('''Select * from user_interests 
+									left join users 
+										on user_interests.USER_ID = users.ROWID
+									left join topics
+										on user_interests.TOPIC_ID = topics.ROWID
+									where users.USERNAME = (?)''', 
+									(user["username"],))
+	user["interests"] = [x[6] for x in user_interests if x[6] != None]
+
+	user_events = c.execute('''Select * from user_events 
+								left join users 
+									on user_events.USER_ID = users.ROWID
+								left join topic_events
+									on topic_events.ROWID = user_events.EVENT_ID
+								where users.USERNAME = (?)''', 
+								(user["username"],))
+	user["events"] = [x[7] for x in user_events if x[7] != None]
+
+	user_friendships = c.execute('''Select * from friendships 
+								left join users as main_users
+									on main_users.ROWID = friendships.ALLOWED_USER_ID
+								left join users as visible_users
+									on visible_users.ROWID = friendships.VISIBLE_USER_ID
+								where visible_users.USERNAME = (?)''', 
+								(user["username"],))
+	user["friendships"] = [x[3] for x in user_friendships if x[3] != None]
 
 def build_topic_from_row(query_row):
 	topic = dict()
@@ -62,7 +88,7 @@ def build_event_from_row(query_row):
 
 @app.route("/users")
 def get_users():
-	users = c.execute("SELECT * FROM users")
+	users = [x for x in c.execute("SELECT * FROM users")]
 	user_rows = [build_user_from_row(x, False) for x in users]
 	return json.dumps(user_rows)
 
@@ -82,7 +108,33 @@ def insert_test_data():
 	test_users = [	('Ian Fade', 'giewev', 1, '444-444-4444'),
 					('David Harupa', 'dave top', 1,'555-555-5555'),
 					('Alex Lopez', 'alex', 1, '666-666-6666')]
+
+	test_topics = [ ('Switch',),
+					('Xbox',),
+					('Hackathons',)]
+
+	test_interests = [	(1,1),
+						(1,2),
+						(2,2)]
+
+	test_events = [	(3, "Hofstra Hack Spring 2017", "Come hack with friends at Hofstra University", '2017-01-01 00:00:00'),
+					(1, "Switch Release", "Gamestop and bestbuy will begin selling switches at midnight", '2017-03-02 23:59:59')]
+
+	test_user_events = [	(1, 1),
+							(2, 1),
+							(3, 1),
+							(2, 2)]
+
+	test_friendships = [(1, 2),
+						(1, 3),
+						(2, 3)]
+
 	c.executemany('INSERT INTO users VALUES (?,?,?,?)', test_users)
+	c.executemany('INSERT INTO topics VALUES (?)', test_topics)
+	c.executemany('INSERT INTO user_interests VALUES (?,?)', test_interests)
+	c.executemany('INSERT INTO topic_events VALUES (?,?,?,?)', test_events)
+	c.executemany('INSERT INTO user_events VALUES (?,?)', test_user_events)
+	c.executemany('INSERT INTO friendships VALUES (?,?)', test_friendships)
 
 if __name__ == "__main__":
 	insert_test_data()
